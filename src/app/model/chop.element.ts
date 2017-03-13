f89cd19e9e9a3ec595422a8fb5fdd956ece03399
Markus Eggimann <eggimannm@outlook.com>
2017-03-13 20:44:28 +0100

    Removed unused model classes.

--- src/app/model/chop.element.ts
+++
@@ -1,9 +0,0 @@
-import { CatalogElement } from "./catalog.element";
-
-/**
- * Class representing an element within the
- * CHOP catalog.
- */
-export class ChopElement extends CatalogElement {
-
-}
