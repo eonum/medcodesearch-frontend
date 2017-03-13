edaa8f968e49b676661f0b4ca0f84842c4ff372f
Bruno Senti <brunosenti@gmail.com>
2017-03-13 17:54:21 +0100

    use html selector for CatalogSelectComponent

--- src/app/components/catalog-select/catalog-select.component.ts
+++ src/app/components/catalog-select/catalog-select.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, Input} from '@angular/core';
 import { Router, ActivatedRoute } from "@angular/router";
 import { SwissDrgCatalog } from "../../catalog/swissdrg.catalog";
 import { SwissDrgService } from "../../service/swissdrg.service";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'catalog-select',
@@ -13,29 +14,29 @@ import { SwissDrgService } from "../../service/swissdrg.service";
 /**
  * Allow user to select a catalog and version.
  * Navigate with /<catalog.name>/<version>/ to
- * CatalogComponent
+ * CatalogComponent.
  */
 export class CatalogSelectComponent implements OnInit {
 
   public drgVersions: string[];
 
   constructor(private router: Router,
-              private route: ActivatedRoute) { }
+              private translate: TranslateService) { }
 
   /**
    * Set catalog versions.
    */
   ngOnInit() {
-    // TODO get async and store with swissDrgCatalog
+    // TODO - get versions from the catalog services
     this.drgVersions = ["V1.0","V2.0","V3.0","V4.0","V5.0","V6.0"]
   }
 
   /**
-   * Navigate to <name>/<version>/
+   * Navigate to /:language/:catalog/:version
    * @param {string} name
    * @param {string} version
    */
   public gotoCatalog(name: string, version: string):void {
-    this.router.navigate([name ,version], { relativeTo: this.route })
+    this.router.navigate([this.translate.currentLang, name ,version])
   }
 }
