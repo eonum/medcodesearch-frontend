80c116ec7cead68c7928127cc18bcc72d5218360
Stefan Serena <serena@increations.eu>
2017-03-15 13:16:08 +0100

    Catalog Select - Style Improvements

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -1,33 +1,32 @@
 import {Component} from '@angular/core';
 
-import { SwissDrgCatalog } from "../../catalog/swissdrg.catalog";
-import { CHOPCatalog } from "../../catalog/chop.catalog";
-import { ICDCatalog } from "../../catalog/icd.catalog";
-import { CatalogService } from "../../service/catalog.service";
-import { CatalogElement } from "../../model/catalog.element";
-import {Catalog} from "../../catalog/catalog";
+import {SwissDrgCatalog} from '../../catalog/swissdrg.catalog';
+import {CHOPCatalog} from '../../catalog/chop.catalog';
+import {ICDCatalog} from '../../catalog/icd.catalog';
+import {CatalogService} from '../../service/catalog.service';
+import {CatalogElement} from '../../model/catalog.element';
 
 @Component({
     selector: 'results',
     templateUrl: './results.component.html',
     styleUrls: ['./results.component.css'],
-    providers: [ SwissDrgCatalog, CHOPCatalog, ICDCatalog, { provide: "ICatalogService", useClass: CatalogService} ]
+    providers: [SwissDrgCatalog, CHOPCatalog, ICDCatalog, {provide: 'ICatalogService', useClass: CatalogService}]
 })
-export class ResultsComponent  {
+export class ResultsComponent {
 
     private searchResults: CatalogElement[];
     private catalogs: any;
 
     constructor(private swissDrgCatalog: SwissDrgCatalog,
-        private chopCatalog: CHOPCatalog,
-        private icdCatalog: ICDCatalog) {
-        
+                private chopCatalog: CHOPCatalog,
+                private icdCatalog: ICDCatalog) {
+
         this.catalogs = {};
         this.catalogs['swissdrg'] = swissDrgCatalog;
         this.catalogs['chop'] = chopCatalog;
         this.catalogs['icd'] = icdCatalog;
     }
-    
+
     updateResults(catalog: string, version: string, query: string) {
 
         this.catalogs[catalog].search(version, query)
@@ -35,10 +34,10 @@ export class ResultsComponent  {
                 this.searchResults = results;
             })
             .catch(error => {
-                this.handleError()
+                this.handleError();
             });
     }
-    
+
     private handleError(): void {
     }
 
--- src/app/components/search-form/search-form.component.ts
+++ src/app/components/search-form/search-form.component.ts
@@ -1,34 +1,34 @@
-import {Component, OnInit, ViewChild } from '@angular/core';
-import {ActivatedRoute, Router, Params} from "@angular/router";
-import { SwissDrgCatalog } from "../../catalog/swissdrg.catalog";
-import { ICDCatalog } from "../../catalog/icd.catalog";
-import { CHOPCatalog } from "../../catalog/chop.catalog";
-import { Catalog } from '../../catalog/catalog';
-import {TranslateService} from "@ngx-translate/core";
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute, Router, Params} from '@angular/router';
+import {SwissDrgCatalog} from '../../catalog/swissdrg.catalog';
+import {ICDCatalog} from '../../catalog/icd.catalog';
+import {CHOPCatalog} from '../../catalog/chop.catalog';
+import {TranslateService} from '@ngx-translate/core';
 import {ResultsComponent} from '../results/results.component';
 
 @Component({
     selector: 'app-search-form',
     templateUrl: './search-form.component.html',
     styleUrls: ['./search-form.component.css'],
-    providers: [ SwissDrgCatalog, CHOPCatalog, ICDCatalog ]
+    providers: [SwissDrgCatalog, CHOPCatalog, ICDCatalog]
 })
+
 export class SearchFormComponent implements OnInit {
 
     catalogs: any;
     catalog: string;
     version: string;
     query: string;
-    
+
     @ViewChild(ResultsComponent)
     private resultsComponent: ResultsComponent;
-    
+
     constructor(private route: ActivatedRoute,
-        private router: Router,
-        private swissDrgCatalog: SwissDrgCatalog,
-        private chopCatalog: CHOPCatalog,
-        private icdCatalog: ICDCatalog,
-        private translate: TranslateService) {
+                private router: Router,
+                private swissDrgCatalog: SwissDrgCatalog,
+                private chopCatalog: CHOPCatalog,
+                private icdCatalog: ICDCatalog,
+                private translate: TranslateService) {
 
         this.catalogs = [
             {
@@ -47,60 +47,58 @@ export class SearchFormComponent implements OnInit {
                 catalog: icdCatalog
             }
         ];
-        
-        for(let n in this.catalogs) {
+
+        for (const n in this.catalogs) {
             this.getVersions(this.catalogs[n]);
         }
-        
     }
 
     /**
      * Preselect the proper catalog version if given through url
      */
     ngOnInit() {
-        
+
         this.route.params.subscribe((params: Params) => {
             this.catalog = params['catalog'];
             this.version = params['version'];
             this.query = params['query'] ? params['query'] : '';
 
             if (this.catalog && this.version) {
-                for(let n in this.catalogs) {
+                for (let n in this.catalogs) {
                     if (this.catalogs[n].path === this.catalog) {
                         this.catalogs[n].currentVersion = this.version;
                     }
                 }
             }
-            
+
             this.resultsComponent.updateResults(this.catalog, this.version, this.query);
-            
+
         });
-        
+
     }
-    
+
     /**
      * Get versions for catalog
      */
     private async getVersions(catalog: any) {
-        let versions = await catalog.catalog.getVersions();
+        const versions = await catalog.catalog.getVersions();
         catalog.versions = versions.reverse();
         if (!catalog.currentVersion) {
             catalog.currentVersion = versions[0];
         }
     }
-    
+
     /**
      * Update based on catalog selections
      */
     public updateCatalog(catalog: string, version: string): void {
         if (this.query) {
             this.router.navigate([this.translate.currentLang, catalog, version, this.query]);
-        }
-        else {
+        } else {
             this.router.navigate([this.translate.currentLang, catalog, version]);
         }
     }
-    
+
     /**
      * Update based on search
      */
