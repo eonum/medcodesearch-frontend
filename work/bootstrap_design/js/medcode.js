93716da6bd5c3c0bf503aebe4820dfc010a1490b
Markus Eggimann <eggimannm@outlook.com>
2017-03-16 08:28:35 +0100

    Use bootstrap built in classes for dropdowns

--- work/bootstrap_design/js/medcode.js
+++ work/bootstrap_design/js/medcode.js
@@ -1,14 +1,3 @@
 $(function() {
-	$('.button .version').on('mouseenter', function() {
-  	$(this).find('ul').show();
-  });
   
-$('.button ul').on('mouseleave', function() {
-    $(this).hide();
-});
-
-  $('.button li').on('click', function(e) {
-  	$(this).find('.version > span').text(e.target.innerText);
-    $(this).find('.button ul').hide();
-  });
 });
