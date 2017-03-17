916bf8b400ca8fcd0c0cb02e37bdd8b9dd0ed47f
Stefan Serena <serena@increations.eu>
2017-03-17 12:05:28 +0100

    SwissDRG Catalog - Cosmetic Changes

--- src/app/catalog/swissdrg.catalog.ts
+++ src/app/catalog/swissdrg.catalog.ts
@@ -1,18 +1,17 @@
-import { Catalog } from './catalog'
-import { Inject, Injectable } from "@angular/core";
-import { CatalogElement } from "../model/catalog.element";
-import { ICatalogService } from "../service/i.catalog.service";
+import { Catalog } from './catalog';
+import { Inject, Injectable } from '@angular/core';
+import { ICatalogService } from '../service/i.catalog.service';
 
 /**
  * Concrete implementation of catalog class for the SwissDRG catalog.
  */
 @Injectable()
 export class SwissDrgCatalog extends Catalog {
-    
-    constructor(@Inject("ICatalogService") service: ICatalogService) {
+
+    constructor(@Inject('ICatalogService') service: ICatalogService) {
         super(service);
-        this.name = "SwissDRG";
-        this.codeRegex = "^[A-Za-z]\\d{1,2}[A-Za-z]{0,1}$";
-        this.elements = [[ "drgs", "adrgs"], [ "drgs", "adrgs", "partition", "mdc" ], "drgs"];
+        this.name = 'SwissDRG';
+        this.codeRegex = '^[A-Za-z]\\d{1,2}[A-Za-z]{0,1}$';
+        this.elements = [[ 'drgs', 'adrgs'], [ 'drgs', 'adrgs', 'partition', 'mdc' ], 'drgs'];
     }
 }
