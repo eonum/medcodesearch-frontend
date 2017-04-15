8f054357e3b47be8ec6801b16c747e3f6f341a86
Markus Eggimann <eggimannm@outlook.com>
2017-04-15 10:31:28 +0200

    Code formatting

--- src/app/helper/sort.helper.ts
+++ src/app/helper/sort.helper.ts
@@ -14,7 +14,7 @@ export class SortHelper {
    */
   public static compareAsLiteral(a: string, b: string): number {
     if (a < b) { return -1; }
-    if (a > b) { return 1; } 
+    if (a > b) { return 1; }
     return 0;
   }
 
