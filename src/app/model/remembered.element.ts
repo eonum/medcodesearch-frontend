73e6e94dd7bc4aaead1344862aae7bf34eab28c0
Markus Eggimann <eggimannm@outlook.com>
2017-04-27 10:25:30 +0200

    Improved design of favorite menu.

--- src/app/model/remembered.element.ts
+++ src/app/model/remembered.element.ts
@@ -8,6 +8,7 @@ export class RememberedElement {
   public type: string;
   public name: string;
   public code: string;
+  public text: string;
 
   public static from(element: CatalogElement, version: string, catalog: string, language: string): RememberedElement {
     const to = new RememberedElement();
@@ -17,6 +18,7 @@ export class RememberedElement {
     to.type = element.type;
     to.name = element.name;
     to.code = element.code;
+    to.text = element.text;
 
     return to;
   }
