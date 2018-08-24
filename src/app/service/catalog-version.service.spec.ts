d392f59bbe28777ac4742d59d1d79a1b3e8747b3
Ramona <ramona.beck@students.unibe.ch>
2018-08-24 10:07:27 +0200

    Fix tests

--- src/app/service/catalog-version.service.spec.ts
+++ src/app/service/catalog-version.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { CatalogVersionService } from './catalog-version.service';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('CatalogVersionService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [CatalogVersionService],
-      imports: [HttpModule],
+      imports: [HttpClientModule],
     });
   });
 
