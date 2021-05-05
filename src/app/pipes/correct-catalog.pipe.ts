74261a0dbd1a7dfeaa221294c9ab404c4acedbb7
waldleben <thea.waldleben@gmail.com>
2021-05-05 23:09:13 +0200

    added small changes

--- src/app/pipes/correct-catalog.pipe.ts
+++ src/app/pipes/correct-catalog.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import {el} from '@angular/platform-browser/testing/src/browser_util';
 
 @Pipe({
   name: 'correctCatalog'
@@ -10,8 +11,7 @@ export class CorrectCatalogPipe implements PipeTransform {
       if (/^KLV1\sV1\s2\d+/.test(s)) {
         s = s.replace(/1\sV1\s/, ' Anhang 1 (01/01/');
         s += ')';
-      }
-      if (/^KLV1\sV2\s2\d+/.test(s)) {
+      } else if (/^KLV1\sV2\s2\d+/.test(s)) {
         s = s.replace(/1\sV2\s/, ' Anhang 1 (01/07/');
         s += ')';
       }
