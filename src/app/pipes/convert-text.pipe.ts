3b083d574da7fefd1195dbc8e5bbe1ff692d511e
waldleben <thea.waldleben@gmail.com>
2021-05-06 14:15:26 +0200

    added small changes for list display to pipe

--- src/app/pipes/convert-text.pipe.ts
+++ src/app/pipes/convert-text.pipe.ts
@@ -6,85 +6,76 @@ import {last} from 'rxjs/operators';
 })
 export class ConvertTextPipe implements PipeTransform {
 
-  /** Get the string value of the group element and check if matches the regex for a list.
-   * If true, add <br> elements for each match.
+  /** Get the string value of the group element and check if string matches the regex for a list.
+   * If true, add <br> or <li> elements for each match.
    * If false, return the original string.
    *
    * @param str: group.text
    * */
   public transform(str: string): string {
-    const regexList = /\n(?=–\s)/g;
+    const regexList = /\n–\s/g;
     const regexListWithNumber = /\n(?=\d\.\s)/g;
-    const regexListWithLetterDot = /\n(?=\w\.\s)/g;
-    const regexListLetterWithBracket = /\n(?=\w\)\s)/g;
+    const regexListWithLetterDot = /\n(?=[a-zA-Z]\.\s)/g;
+    const regexListLetterWithBracket = /\n(?=[a-zA-Z]\)\s)/g;
 
     const regexListStart = /^–\s/;
     const regexListStartsWithNumber = /^\d\.\s/;
     const regexListStartsWithLetter = /^\w\)\s/;
 
-   if (regexListStartsWithNumber.test(str) || regexListWithNumber.test(str)) {
+    if (regexListStartsWithLetter.test(str) || regexListLetterWithBracket.test(str)) {
+      str =  str.replace(regexListLetterWithBracket, '<br>');
+      if (regexListStartsWithNumber.test(str) || regexListWithNumber.test(str)) {
+        str =  str.replace(regexListWithNumber, '<br>');
+        if (regexList.test(str)) {
+          const lastElement = str.split(regexList);
+          const el = lastElement[lastElement.length - 1]
+          if (el.length < 130) {
+            str =  str.replace(/\n(?=–\s)/g, '<br>');
+          }
+        }
+      } else if (regexList.test(str)) {
+        const lastElement = str.split(regexList);
+        const el = lastElement[lastElement.length - 1]
+        if (el.length < 130) {
+          str =  str.replace(/\n–\s/, '<ul><li>').replace(regexList, '</li><li>');
+          str += '</li></ul>';
+        }
+      }
+    } else if (regexListStartsWithNumber.test(str) || regexListWithNumber.test(str)) {
+      if (regexListStartsWithNumber.test(str)) {
+        str =  str.replace(regexListWithNumber, '<br>');
+      } else {
+        str =  str.replace(/\n\d\.\s/, '<ol><li>').replace(/\n\d\.\s/g, '</li><li>');
+        str += '</li></ol>';
+      }
      if (regexListStartsWithLetter.test(str) || regexListWithLetterDot.test(str)) {
-       if (regexListStart.test(str)) {
-         str = str.replace(regexList, '<br>');
-       } else if (regexList.test(str)) {
+       str =  str.replace(regexListWithLetterDot, '<br>');
+       if (regexList.test(str)) {
          const lastElement = str.split(regexList);
          const el = lastElement[lastElement.length - 1]
          if (el.length < 130) {
-           str = str.replace(regexList, '<br>');
+           str =  str.replace(/\n(?=–\s)/g, '<br>');
          }
        }
-       str = str.replace(regexListWithLetterDot, '<br>');
-     } else if (regexListStart.test(str)) {
-       str = str.replace(regexList, '<br>');
-     } else if (regexList.test(str)) {
+     } if (regexList.test(str)) {
        const lastElement = str.split(regexList);
        const el = lastElement[lastElement.length - 1]
        if (el.length < 130) {
-         str = str.replace(regexList, '<br>');
+         str =  str.replace(/\n–\s/, '<ul><li>').replace(regexList, '</li><li>');
+         str += '</li></ul>';
        }
      }
-     str =  str.replace(regexListWithNumber, '<br>');
-   } else if (regexListStartsWithLetter.test(str) || regexListLetterWithBracket.test(str)) {
-     if (regexListStartsWithNumber.test(str) || regexListWithNumber.test(str)) {
-       if (regexListStart.test(str)) {
-         str = str.replace(regexList, '<br>');
-       } else if (regexList.test(str)) {
-         const lastElement = str.split(regexList);
-         const el = lastElement[lastElement.length - 1]
-         if (el.length < 130) {
-           str = str.replace(regexList, '<br>');
-         }
-       }
-       str = str.replace(regexListWithNumber, '<br>');
-     } else if (regexListStart.test(str)) {
-       str = str.replace(regexList, '<br>');
-     } else if (regexList.test(str)) {
-       const lastElement = str.split(regexList);
-       const el = lastElement[lastElement.length - 1]
-       if (el.length < 130) {
-         str = str.replace(regexList, '<br>');
-       }
-     }
-     str =  str.replace(regexListLetterWithBracket, '<br>');
    } else if (regexListStart.test(str)) {
-     str =  str.replace(regexList, '<br>');
+     str =  str.replace(regexListStart, '<ul><li>').replace(regexList, '</li><li>');
+     str += '</li></ul>';
    } else if (regexList.test(str)) {
      const lastElement = str.split(regexList);
      const el = lastElement[lastElement.length - 1]
      if (el.length < 130) {
-       str =  str.replace(regexList, '<br>');
+       str =  str.replace(/\n–\s/, '<ul><li>').replace(regexList, '</li><li>');
+       str += '</li></ul>';
      }
    }
    return str;
   }
-
-  /*
-  private listHtml (str: string): any {
-    const s = str.split(/\n–\s/g);
-    const list = [];
-    for (const obj of s) {
-     list.push('<li>' + obj + '</li>');
-    }
-    return list;
-  }*/
 }
